88d77507533b594e08fc31bcfed666bf1d34b7f3
Hideyuki Matsui <atagohan@yahoo.co.jp>
2025-11-22 12:23:51 +0900

    REST APIでランダムで名言の取得ができるように

--- api/rest-api.php
+++ api/rest-api.php
@@ -1,4 +1,38 @@
 <?php
+function qc_prepare_quote_response($post) {
+    $book = get_post_meta($post->ID, '_qc_quote_book', true);
+    $author = $book ? get_post_meta($book, '_qc_book_author', true) : null;
+    $purchase_link = $book ? get_post_meta($book, '_qc_purchase_link', true) : null;
+
+    $book_data = null;
+    if ($book) {
+        $author_data = null;
+        if ($author) {
+            $author_data = [
+                'id' => $author,
+                'title' => get_the_title($author),
+                'permalink' => get_permalink($author),
+            ];
+        }
+
+        $book_data = [
+            'id' => $book,
+            'title' => get_the_title($book),
+            'permalink' => get_permalink($book),
+            'purchase_link' => $purchase_link ? esc_url_raw($purchase_link) : null,
+            'author' => $author_data,
+        ];
+    }
+
+    return [
+        'id' => $post->ID,
+        'title' => $post->post_title,
+        'content' => wpautop($post->post_content),
+        'permalink' => get_permalink($post->ID),
+        'book' => $book_data,
+    ];
+}
+
 add_action('rest_api_init', function () {
 
     register_rest_route('quotes/v1', '/list', [
@@ -9,42 +43,24 @@ add_action('rest_api_init', function () {
                 'numberposts' => -1
             ]);
 
-            $data = [];
-            foreach ($posts as $p) {
-                $book = get_post_meta($p->ID, '_qc_quote_book', true);
-                $author = $book ? get_post_meta($book, '_qc_book_author', true) : null;
-                $purchase_link = $book ? get_post_meta($book, '_qc_purchase_link', true) : null;
+            return array_map('qc_prepare_quote_response', $posts);
+        }
+    ]);
 
-                $book_data = null;
-                if ($book) {
-                    $author_data = null;
-                    if ($author) {
-                        $author_data = [
-                            'id' => $author,
-                            'title' => get_the_title($author),
-                            'permalink' => get_permalink($author),
-                        ];
-                    }
+    register_rest_route('quotes/v1', '/random', [
+        'methods' => 'GET',
+        'callback' => function () {
+            $posts = get_posts([
+                'post_type' => 'quotes',
+                'numberposts' => 1,
+                'orderby' => 'rand',
+            ]);
 
-                    $book_data = [
-                        'id' => $book,
-                        'title' => get_the_title($book),
-                        'permalink' => get_permalink($book),
-                        'purchase_link' => $purchase_link ? esc_url_raw($purchase_link) : null,
-                        'author' => $author_data,
-                    ];
-                }
-
-                $data[] = [
-                    'id' => $p->ID,
-                    'title' => $p->post_title,
-                    'content' => wpautop($p->post_content),
-                    'permalink' => get_permalink($p->ID),
-                    'book' => $book_data,
-                ];
+            if (empty($posts)) {
+                return [];
             }
 
-            return $data;
+            return qc_prepare_quote_response($posts[0]);
         }
     ]);
 
