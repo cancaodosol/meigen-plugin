ba784d9a1f6291d52e8c257521ab95e7a23703bc
Hideyuki Matsui <atagohan@yahoo.co.jp>
2025-11-22 12:20:56 +0900

    REST APIの名言情報を補填

--- api/rest-api.php
+++ api/rest-api.php
@@ -13,13 +13,34 @@ add_action('rest_api_init', function () {
             foreach ($posts as $p) {
                 $book = get_post_meta($p->ID, '_qc_quote_book', true);
                 $author = $book ? get_post_meta($book, '_qc_book_author', true) : null;
+                $purchase_link = $book ? get_post_meta($book, '_qc_purchase_link', true) : null;
+
+                $book_data = null;
+                if ($book) {
+                    $author_data = null;
+                    if ($author) {
+                        $author_data = [
+                            'id' => $author,
+                            'title' => get_the_title($author),
+                            'permalink' => get_permalink($author),
+                        ];
+                    }
+
+                    $book_data = [
+                        'id' => $book,
+                        'title' => get_the_title($book),
+                        'permalink' => get_permalink($book),
+                        'purchase_link' => $purchase_link ? esc_url_raw($purchase_link) : null,
+                        'author' => $author_data,
+                    ];
+                }
 
                 $data[] = [
                     'id' => $p->ID,
                     'title' => $p->post_title,
                     'content' => wpautop($p->post_content),
-                    'book' => $book ? get_the_title($book) : null,
-                    'author' => $author ? get_the_title($author) : null,
+                    'permalink' => get_permalink($p->ID),
+                    'book' => $book_data,
                 ];
             }
 
